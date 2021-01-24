b2dfc6027c3c38052283b0d7a7accecbf8b64769
km3xy <77920834+km3xy@users.noreply.github.com>
2021-01-24 19:37:56 +0800

    Initial commit

---
+++ y9_km3xy_灯光随机指令触发事件版.php
@@ -0,0 +1,445 @@
+
+
+
+   <label>
+ 
+ 
+ 
+
+<h1>您随机的灯光指令是____<span id="y1">_____</span></h1>
+
+   </label>
+ 
+ 
+ 
+<h1>您随机的灯光指令是____<span id="y2">_____</span></h1>
+
+   </label>
+ 
+ 
+ 
+ 
+<h1>您随机的灯光指令是____<span id="y3">_____</span></h1>
+
+   </label>
+   
+   
+   
+   
+<h1>您随机的灯光指令是____<span id="y4">_____</span></h1>
+
+   </label>
+   
+   
+
+ 
+<h1>您随机的灯光指令是____<span id="y5">_____</span></h1>
+
+   </label>
+ 
+ 
+
+ 
+
+
+<h1>您随机的灯光指令是____<span id="spanId2">_____</span></h1>
+
+   </label>
+ 
+
+
+
+
+<h1>您随机的灯光指令是____<span id="spanId3">_____</span></h1>
+
+
+
+   </label>
+ 
+
+
+
+
+
+<script>
+        $(function () {
+            $("input[id=abc").click(function () {
+                var arr = [];
+                $("input[id=abc").each(function () {
+                    if(this.checked){
+                        arr.push(this.value);
+                    }else{
+                        var index = arr.indexOf(this.value);
+                        if(index != -1){
+                            arr[index] = "";
+                        }
+                    }
+                });
+                
+    
+    
+                
+var  y=Math.floor(Math.random()*0+0); 
+
+            
+var  y3="、"; 
+
+                
+                
+var colorsxy = ['y1','y2'];
+
+
+
+var xy = colorsxy[(Math.floor(Math.random() * (colorsxy.length)))]
+
+var y2="+";
+
+
+
+var  y1="#近光状态#";
+
+
+var  y2="#远光状态#";
+
+
+
+ 
+var colorsaayy=[y1,y2,y3,y4,y5];
+
+
+
+
+var yy2=colorsaayy[(Math.floor(Math.random() * (colorsaayy.length)))]
+
+
+
+var a="夜间通过拱桥";
+
+
+var b="夜间通过坡路";
+
+
+var c="夜间通过急弯";
+
+
+var d="夜间通过人行横道";
+
+
+
+var e="夜间通过没有交通信号灯控制的路口";
+
+
+
+var f="夜间超越前方车辆";
+
+
+
+var g="远近光交替题，#灯光状态#:在远光状态上，先变回近光再做远近光交替";
+
+
+var g2="远近光交替题，#灯光状态#在近光状态上，直接做远近光交替";
+
+
+
+
+var h="夜间与机动车会车";
+
+
+var i="夜间通过有交通信号灯控制的路口";
+
+
+
+var j="夜间同方向近距离跟车行驶";
+
+
+
+var k="夜间在照明良好的道路上行驶";
+
+
+
+
+var l="夜间在有路灯的道路上行驶";
+
+
+
+
+var m="/*夜间直行通过路口*/";
+
+
+
+
+var n="近光题，#灯光状态#在远光状态上变回近光";
+
+var n2="近光题，#灯光状态#在近光状态上不用动";
+
+
+
+var o="路边临时停车";
+
+
+var p="路边临时停车,需要开启小灯,双闪";
+
+
+
+var p2="(注意路边临时停车，#灯光状态#,如果在远光灯上听到以上语音,需要先变回近光灯再开启小灯双闪)";
+
+
+
+var p3="(注意路边临时停车#灯光状态#,如果在近光灯上听到以上语音,开启小灯双闪即可)";
+
+
+
+
+var q="/ * 夜间在道路上发生交通事故(故障)妨碍交通难以移动 */";
+
+
+
+var r="夜间在没有路灯照明不良条件下行驶";
+
+
+
+var r2="夜间在没有路灯照明不良条件下行驶(注意,仅此一句需要将灯光停留在远光灯上)";
+
+
+
+
+var s="没有路灯";
+
+
+var t="有路灯";
+
+
+var u="没有交通信号灯";
+
+
+var v="路口直行，直行通过";
+
+
+var w="通过路口";
+
+
+var x="超越前方车辆";
+
+
+var y="近距离跟车";
+
+
+var z="模拟夜间考试完成请关闭所有灯光请起步继续完成考试";
+
+
+
+var colorsxy = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
+
+
+
+
+
+var xy2 = colorsxy[(Math.floor(Math.random() * (colorsxy.length)))]
+
+var y2="+";
+
+ 
+                
+                $("#"+xy).text(arr.join(",")+y2+y3+xy2+yy2);
+   
+            })
+        });
+</script>
+    
+    
+    
+<script>
+        $(function () {
+            $("input[id=abu").click(function () {
+                var arr = [];
+                $("input[id=abu").each(function () {
+                    if(this.checked){
+                        arr.push(this.value);
+                    }else{
+                        var index = arr.indexOf(this.value);
+                        if(index != -1){
+                            arr[index] = "";
+                        }
+                    }
+                });
+                
+                
+                
+var  yy=Math.floor(Math.random()*0+0); 
+
+            
+var  y3="、"; 
+
+                
+                
+var colorsyx = ['spanId2','spanId3'];
+
+
+
+var xyz = colorsyx[(Math.floor(Math.random() * (colorsyx.length)))]
+
+
+
+var y2="+";
+
+
+
+
+var  y1="#近光状态#";
+
+
+var  y2="#远光状态#";
+
+
+
+ 
+var colorsaayy=[y1,y2];
+
+
+
+
+var yy2=colorsaayy[(Math.floor(Math.random() * (colorsaayy.length)))]
+
+
+
+var a="夜间通过拱桥";
+
+
+var b="夜间通过坡路";
+
+
+var c="夜间通过急弯";
+
+
+var d="夜间通过人行横道";
+
+
+
+var e="夜间通过没有交通信号灯控制的路口";
+
+
+
+var f="夜间超越前方车辆";
+
+
+
+
+var g="远近光交替题，#灯光状态#:在远光状态上，先变回近光再做远近光交替";
+
+
+
+var g2="远近光交替题，#灯光状态#在近光状态上，直接做远近光交替";
+
+
+
+
+var h="夜间与机动车会车";
+
+
+var i="夜间通过有交通信号灯控制的路口";
+
+
+
+var j="夜间同方向近距离跟车行驶";
+
+
+
+var k="夜间在照明良好的道路上行驶";
+
+
+
+
+var l="夜间在有路灯的道路上行驶";
+
+
+
+
+var m="/*夜间直行通过路口*/";
+
+
+
+
+var n="近光题，#灯光状态#在远光状态上变回近光";
+
+var n2="近光题，#灯光状态#在近光状态上不用动";
+
+
+
+var o="路边临时停车";
+
+
+var p="路边临时停车,需要开启小灯,双闪";
+
+
+
+var p2="(注意路边临时停车，#灯光状态#,如果在远光灯上听到以上语音,需要先变回近光灯再开启小灯双闪)";
+
+
+
+var p3="(注意路边临时停车#灯光状态#,如果在近光灯上听到以上语音,开启小灯双闪即可)";
+
+
+
+
+var q="/ * 夜间在道路上发生交通事故(故障)妨碍交通难以移动 */";
+
+
+
+var r="夜间在没有路灯照明不良条件下行驶";
+
+
+
+var r2="夜间在没有路灯照明不良条件下行驶(注意,仅此一句需要将灯光停留在远光灯上)";
+
+
+
+
+var s="没有路灯";
+
+
+var t="有路灯";
+
+
+var u="没有交通信号灯";
+
+
+var v="路口直行，直行通过";
+
+
+var w="通过路口";
+
+
+var x="超越前方车辆";
+
+
+var y="近距离跟车";
+
+
+var z="模拟夜间考试完成请关闭所有灯光请起步继续完成考试";
+
+
+
+
+
+var colorsxy = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
+
+
+
+var xy2 = colorsxy[(Math.floor(Math.random() * (colorsxy.length)))]
+
+var y2="+";
+
+ 
+
+                
+                $("#"+xyz).text(arr.join(",")+y2+yy+y3+xy2+yy2);
+                
+
+            })
+        });
+
+
+
+</script>
+    
+    
